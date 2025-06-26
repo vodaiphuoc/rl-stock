dec597cd3ce811ac436229bf75e76613d12116b1
vodaiphuoc <phuocvodn98@gmail.com>
2025-06-27 02:01:56 +0700

    add symbols

--- get_data.py
+++ get_data.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 
 
-def get_stock(stock_symbol: str, start_day:str = "2022-01-01")->pd.DataFrame:
+def get_stock(stock_symbol: str, start_day:str = "2010-01-01")->pd.DataFrame:
     acb_stocks = Vnstock().stock(symbol=stock_symbol, source= "VCI")
     stock_values =  acb_stocks.quote.history(
         start = start_day, 
@@ -23,19 +23,26 @@ def get_stock(stock_symbol: str, start_day:str = "2022-01-01")->pd.DataFrame:
     stock_values['month'] = stock_values['time'].dt.month
     stock_values['day'] = stock_values['time'].dt.day
 
-    print('length before: ', len(stock_values))
     stock_values.drop_duplicates(inplace=True)
     stock_values.reset_index(drop = True, inplace = True)
-    print('length after: ', len(stock_values))
 
     stock_values['time'] = pd.to_datetime(stock_values['time'], format="%Y-%m-%d")
-    stock_values['time_diff'] = stock_values['time'].diff()
-
     return stock_values
 
-companies = ["VCB","ACB","TCB","BID","FOC","FPT","HPG"]
 
 
-for symbol in companies:
-    data = get_stock(stock_symbol= symbol, start_day="2015-01-01")
-    data.to_excel(f"data/prices/{symbol}.xlsx")
+companies = [
+    "AAV","CMG","VCB","ACB","TCB",
+    "BID","FOC","FPT","HPG","GEX",
+    "MSR","MVN","YEG","HSG","VHM",
+    "MSN","VIC","PNJ","EIB","STB"
+]
+
+with pd.ExcelWriter('data/prices/all_prices.xlsx', engine='xlsxwriter') as writer:
+    for symbol in companies:
+        try:
+            data = get_stock(stock_symbol= symbol, start_day="2010-01-01")
+            data.to_excel(writer, sheet_name= symbol, index=False)
+        except Exception as e:
+            print(f'error {e} with symbol: {symbol}')
+        
