896b16a41dc4ca72500e3e5c706894b3475b3f3f
vodaiphuoc <phuocvodn98@gmail.com>
2025-07-05 19:40:33 +0700

    update

--- install-TA.sh
+++ install-TA.sh
@@ -1,10 +1,10 @@
 PREFIX=/kaggle/working
 TA_LIBRARY_PATH=$PREFIX/lib
 TA_INCLUDE_PATH=$PREFIX/include
-TARGET_FOLDER=$PWD/ta-lib-0.6.4
+TARGET_FOLDER=ta-lib-0.6.4
 
 wget -q https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
-tar zxf ta-lib-0.6.4-src.tar.gz -C $TARGET_FOLDER
+tar zxf ta-lib-0.6.4-src.tar.gz
 cd $TARGET_FOLDER && \
     ./configure --prefix=$PREFIX && \
     make  && \
