6be1a9686cba42fd880395d982bf72e1465d6638
vodaiphuoc <phuocvodn98@gmail.com>
2025-07-05 18:42:21 +0700

    update

--- install-TA.sh
+++ install-TA.sh
@@ -1,8 +1,9 @@
 PREFIX=/usr
+TA_LIBRARY_PATH=$PREFIX/lib
+TA_INCLUDE_PATH=$PREFIX/include
+
 wget -q https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
 tar zxf ta-lib-0.6.4-src.tar.gz
 cd ta-lib-0.6.4 && ./configure --prefix=$PREFIX
 cd ta-lib-0.6.4 && make  && sudo make install
-export TA_LIBRARY_PATH=$PREFIX/lib
-export TA_INCLUDE_PATH=$PREFIX/include
-pip install TA-Lib
+export TA_LIBRARY_PATH=$TA_LIBRARY_PATH && export TA_INCLUDE_PATH=$TA_INCLUDE_PATH && pip install TA-Lib
